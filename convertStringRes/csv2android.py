d07df0155649beb794167317189a20e9dfba9730
zbl <baoliang.zhang@wuliangroup.com>
2017-05-23 10:45:31 +0800

    国际化字符串文件转换脚本，csv转ios和android

--- convertStringRes/csv2android.py
+++ convertStringRes/csv2android.py
@@ -2,20 +2,29 @@
 # -*- coding: utf-8 -*-
 import csv
 import codecs
+import os
 
 rootPath = "./"
-inputFile = "strings.csv"
-outputFile = "strings.xml"
+inputFile = rootPath + "v6Strings.csv"
+targetPath = "E:/WorkProject/SmartHomeV6Code_andriod/SmartHome/src/main/res/"
+# targetPath = rootPath
+outputFileData = [["values/", "strings.xml", 3], ["values-zh-rCN/", "strings.xml", 4]]
 
-csv_reader = csv.reader(open(rootPath + inputFile, encoding='utf-8'))
-with codecs.open(outputFile, 'w', "utf-8") as out:
-    s = 0
-    out.write("<resources>\n")
-    for row in csv_reader:
-        s += 1
-        if s == 1:
-            continue
-        out.write("  <string name=\"%s\">%s</string>" % ((row[0]), row[2]))
-        out.write("\n")
-    out.write("</resources>")
-    out.flush()
+for outputFile in outputFileData:
+    targetFilePath = targetPath + outputFile[0]
+    targetFile = targetFilePath + outputFile[1]
+    column_index = outputFile[2]
+    if not os.path.exists(targetFilePath):
+        os.makedirs(targetFilePath)
+    with codecs.open(targetFile, 'w', "utf-8") as out:
+        s = 0
+        csv_reader = csv.reader(open(inputFile, encoding='GBK'))
+        out.write("<resources>\n")
+        for row in csv_reader:
+            s += 1
+            if s == 1:
+                continue
+            out.write("  <string name=\"%s\">%s</string>" % ((row[1]), row[column_index]))
+            out.write("\n")
+        out.write("</resources>")
+        out.flush()
--- convertStringRes/csv2ios.py
+++ convertStringRes/csv2ios.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import csv
 import codecs
+import os
 
 rootPath = "./"
-inputFile = "strings.csv"
-outputFile = "Localizable.strings"
+inputFile = rootPath + "v6Strings.csv"
+targetPath = "E:/WorkProject/iosV6/"
+# targetPath = rootPath
+outputFileData = [["en.lproj/", "Localizable.strings", 3], ["zh-Hans.lproj/", "Localizable.strings", 4]]
 
-csv_reader = csv.reader(open(rootPath + inputFile, encoding='utf-8'))
-with codecs.open(outputFile, 'w', "utf-8") as out:
-    s = 0
-    for row in csv_reader:
-        s += 1
-        if s == 1:
-            continue
-        out.write("\"%s=   \"%s\"" % ((row[0] + "\"").ljust(60), row[2]))
-        out.write("\n")
-    out.flush()
+for outputFile in outputFileData:
+    targetFilePath = targetPath + outputFile[0]
+    targetFile = targetFilePath + outputFile[1]
+    column_index = outputFile[2]
+    if not os.path.exists(targetFilePath):
+        os.makedirs(targetFilePath)
+    with codecs.open(targetFile, 'w', "utf-8") as out:
+        s = 0
+        csv_reader = csv.reader(open(inputFile, encoding='GBK'))
+        for row in csv_reader:
+            s += 1
+            if s == 1:
+                continue
+            out.write("\"%s=   \"%s\"" % ((row[1] + "\"").ljust(60), row[column_index]))
+            out.write("\n")
+        out.flush()
