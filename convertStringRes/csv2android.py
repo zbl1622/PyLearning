d1647113a0655df065f185fbc281217be16eb3a6
zbl <baoliang.zhang@wuliangroup.com>
2017-05-24 11:33:29 +0800

    android脚本更新

--- convertStringRes/csv2android.py
+++ convertStringRes/csv2android.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+#表结构 id,group,Key,Translatable,Values,Values-zh-rCN
 import csv
 import codecs
 import os
@@ -9,9 +11,11 @@ inputFile = rootPath + "v6Strings.csv"
 targetPath = "E:/WorkProject/SmartHomeV6Code_andriod/SmartHome/src/main/res/"
 # targetPath = rootPath
 outputFileData = [
-    ["values/", "strings.xml", 3],
-    ["values-zh-rCN/", "strings.xml", 4]
+    ["values/", "strings.xml", 4],
+    ["values-zh-rCN/", "strings.xml", 5]
 ]
+moduleIndex = 1
+keyIndex = 2
 
 for outputFile in outputFileData:
     targetFilePath = targetPath + outputFile[0]
@@ -22,12 +26,30 @@ for outputFile in outputFileData:
     with codecs.open(targetFile, 'w', "utf-8") as out:
         s = 0
         csv_reader = csv.reader(open(inputFile, encoding='GBK'))
-        out.write("<resources>\n")
+
+        dataMap = {}
         for row in csv_reader:
             s += 1
             if s == 1:
                 continue
-            out.write("  <string name=\"%s\">%s</string>" % ((row[1]), row[column_index]))
+            moduleName = row[moduleIndex]
+            dataList = []
+            if moduleName in dataMap:
+                dataList = dataMap[moduleName]
+            else:
+                dataMap[moduleName] = dataList
+            dataList.append(row)
+
+        out.write("<resources>\n")
+        keysList = list(dataMap.keys())
+        keysList.sort()
+        for moduleName in keysList:
+            dataList = dataMap[moduleName]
             out.write("\n")
+            out.write("  <!-- %s -->" % (moduleName))
+            out.write("\n")
+            for row in dataList:
+                out.write("  <string name=\"%s\">%s</string>" % ((row[2]), row[column_index]))
+                out.write("\n")
         out.write("</resources>")
         out.flush()
