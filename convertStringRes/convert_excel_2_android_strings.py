a199ae99a3efefc23e422b4aa52ba18f5afc1609
zbl <baoliang.zhang@wuliangroup.com>
2018-02-09 13:39:01 +0800

    android直接转换excel脚本更新

--- convertStringRes/convert_excel_2_android_strings.py
+++ convertStringRes/convert_excel_2_android_strings.py
@@ -4,26 +4,35 @@ import codecs
 import os
 
 target_path = 'D:/WorkProjects/SmartHomeV6Code_andriod_develop/SmartHome/src/main/res/'
-source_file = './APP国际化翻译字符汇总.xlsx'
+source_file = './APP国际化翻译字符重构汇总.xlsx'
 output_strcut = [
-    ["values", 5],
-    ["values-zh-rCN", 4]
+    ["values", 0, "Wulian-EN"],
+    ["values-zh-rCN", 0, "Wulian-CN"]
 ]
 key_list = list()
 
 
 def convert_safe_string(text):
-    s = str(text).replace('<font>', '[font]').replace('</font>', '[/font]')\
-        .replace('\n', '\\n')\
-        .replace('\'', '\\\'')\
+    s = str(text).replace('<font>', '[font]').replace('</font>', '[/font]') \
+        .replace('\n', '\\n') \
+        .replace('\'', '\\\'') \
         .replace('&', '&#038;')
     return re.sub('%\D|%$', lambda x: "%%" + x.group()[1:], s)
 
 
 def read_excel():
-    global key_list
+    global key_list, output_strcut
     data = xlrd.open_workbook(source_file)
     table = data.sheets()[0]
+
+    # 初始化翻译语言对应的列数
+    head_row = table.row_values(0)
+    for output_data in output_strcut:
+        for i in range(0, len(head_row)):
+            if output_data[2] == head_row[i]:
+                output_data[1] = i
+                break
+
     nrows = table.nrows
     key_list = list()
     for i in range(1, nrows):
@@ -40,7 +49,7 @@ def read_excel():
 
 
 def convert_2_xml():
-    global key_list
+    global key_list, output_strcut
     for f in output_strcut:
         path = f[0]
         if not os.path.exists(target_path + path):
